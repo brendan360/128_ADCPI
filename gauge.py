0afc7fdfaf687b9067393f1a247ce30a5fcccf08
brendan360 <67534108+brendan360@users.noreply.github.com>
2024-06-15 16:55:45 +1000

    Update gauge.py

--- gauge.py
+++ gauge.py
@@ -8,6 +8,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import time
 import os
+import sys
 import math
 from tabulate import tabulate
 sys.path.append('..')
