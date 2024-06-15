64db84c68bd4dfa6b515ca8807c9e98065c65701
brendan360 <67534108+brendan360@users.noreply.github.com>
2024-06-15 21:54:57 +1000

    Update gauge.py

--- gauge.py
+++ gauge.py
@@ -352,8 +352,9 @@ def FUNCT_updateValues():
 #     MAIN          #
 #                   #
 ##################### 
-firstBoot()
-#while True:
+
+while True:
+    firstBoot()
 #    FUNCT_updateValues()
 #    FUNCT_cliPrint()
 #    time.sleep(.2)
