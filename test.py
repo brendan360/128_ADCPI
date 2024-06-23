2c2c742ab45880a02c627aefdcb0bf45c3073db3
brendan360 <67534108+brendan360@users.noreply.github.com>
2024-06-23 20:08:09 +1000

    Update test.py

--- test.py
+++ test.py
@@ -69,8 +69,8 @@ angle = value_to_angle(VALUE)
 needle_length = RADIUS - 8  # Adjusted length
 end_x = CENTER_X + needle_length * math.cos(math.radians(angle))
 end_y = CENTER_Y + needle_length * math.sin(math.radians(angle))
-outline_end_x = CENTER_X + (needle_length + 5) * math.cos(math.radians(angle))
-outline_end_y = CENTER_Y + (needle_length + 5) * math.sin(math.radians(angle))
+outline_end_x = CENTER_X + (needle_length + 1) * math.cos(math.radians(angle))
+outline_end_y = CENTER_Y + (needle_length + 1) * math.sin(math.radians(angle))
 draw.line((CENTER_X, CENTER_Y, outline_end_x, outline_end_y), fill='white', width=outline_width) 
 draw.line((CENTER_X, CENTER_Y, end_x, end_y), fill='red', width=8)
 
