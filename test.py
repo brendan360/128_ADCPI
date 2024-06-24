32140b1d0ad19718ee36c0896d957e0f96a4c76d
brendan360 <67534108+brendan360@users.noreply.github.com>
2024-06-24 20:15:27 +1000

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 from PIL import Image, ImageDraw, ImageFont
 import time
 import sys
-import math
 sys.path.append('..')
 
 from lib import LCD_1inch28
 
-# Define gauge items
+# Define the menus
+level1_menu = ["Gauges", "QuadTemp", "Triple Stack", "Config"]
+config_menu = ["ipaddress", "reboot pi"]
 gaugeItems = {
     "FUEL_PRESSURE": ["1", "Fuel Pres.", 1, 10, 15, 99, 110, 0, 150, "Kpa", 0],
     "BOOST": ["2", "Boost", 1, 0, -19, 24, 28, -20, 30, "psi", 0],
@@ -17,6 +18,7 @@ gaugeItems = {
     "OIL_TEMP": ["7", "Oil °C", 1, 10, 15, 99, 110, 0, 150, "°C", 0],
     "WIDEBAND02": ["8", "O2 AFR", 1, .5, 1, 1.5, 2, 0, 4, "A/F", 0]
 }
+gauge_keys = list(gaugeItems.keys())
 
 # Define colors
 BACKGROUND_COLOR = (30, 30, 30)
@@ -43,8 +45,9 @@ disp.Init()
 # Constants for 240x240 screen
 WIDTH, HEIGHT = 240, 240
 
-# Initialize starting position for scrolling
-start_pos = 0
+# Menu state
+current_menu = "level1"
+current_index = 0
 
 # Main loop
 while True:
@@ -52,16 +55,24 @@ while True:
     image = Image.new('RGB', (WIDTH, HEIGHT), color=BACKGROUND_COLOR)
     draw = ImageDraw.Draw(image)
 
+    # Get the current menu items based on the menu state
+    if current_menu == "level1":
+        menu_items = level1_menu
+    elif current_menu == "config":
+        menu_items = config_menu
+    elif current_menu == "gauges":
+        menu_items = [gaugeItems[key][1] for key in gauge_keys]
+    
     # Calculate vertical position of selected item
     selected_y = HEIGHT // 2 - 25
 
     # Draw menu items
     for i in range(5):
         # Calculate index of current item
-        index = (start_pos + i) % len(gaugeItems)
+        index = (current_index + i) % len(menu_items)
 
         # Calculate text position
-        text = gaugeItems[list(gaugeItems.keys())[index]][1]
+        text = menu_items[index]
         text_color = TEXT_COLOR_SELECTED if i == 2 else TEXT_COLOR_NON_SELECTED1 if i == 1 or i == 3 else TEXT_COLOR_NON_SELECTED2
         text_font = large_font if i == 2 else font if i == 1 or i == 3 else smallfont
         text_bbox = draw.textbbox((0, 0), text, font=text_font)
@@ -74,8 +85,8 @@ while True:
     # Show image on display
     disp.ShowImage(image)
 
-    # Increment start position for scrolling effect
-    start_pos = (start_pos + 1) % len(gaugeItems)
+    # Increment current index for scrolling effect
+    current_index = (current_index + 1) % len(menu_items)
 
     # Delay for scrolling effect
     time.sleep(0.5)
