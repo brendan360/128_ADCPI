ab798b67d89c47a556b725edbc4a82cac45d30fe
brendan360 <67534108+brendan360@users.noreply.github.com>
2024-09-29 16:29:11 +1000

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,22 @@ import signal
 import random
 import socket
 from lib import LCD_1inch28
-from ADCPi import ADCPi
+
+try:
+    from ADCPi import ADCPi
+except ImportError:
+    print("Failed to import ADCPi from python system path")
+    print("Importing from parent folder instead")
+    try:
+        import sys
+        sys.path.append('..')
+        from ADCPi import ADCPi
+    except ImportError:
+        raise ImportError(
+            "Failed to import library from parent folder")
+
+adc = ADCPi(0x68, 0x69, 12)
+
 
 
 ########################
